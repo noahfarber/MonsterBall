6fdf6aaea93df4a9d73e7eb5e7c5ba24364d20ba
noahfarber <Noah.Farber@bluberi.com>
2022-04-11 18:06:22 -0700

    Updates camera scaler to base off of widest screen dimension and reduces minimum screen amount

--- Assets/CameraScaler.cs
+++ Assets/CameraScaler.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 public class CameraScaler : MonoBehaviour
 {
     private Camera _Camera;
-    private float _SmallestScreenWidth = 2208f;
-    private float _OriginalOrthoSize = 5.4f;
+    private float _SmallestScreenWidth = 900f;
+    private float _OriginalOrthoSize = 6f;
 
     // Start is called before the first frame update
     void Start()
     {
         _Camera = GetComponent<Camera>();
-        _Camera.orthographicSize = (Screen.width / _SmallestScreenWidth) * _OriginalOrthoSize;
+        float width = Screen.width >= Screen.height ? Screen.width : Screen.height;
+        _Camera.orthographicSize = (width / _SmallestScreenWidth) * _OriginalOrthoSize;
+
+        Debug.Log("Setting ortho size (" + _Camera.orthographicSize + ") Found Width: " + width + "    Screen Width: " + Screen.width + "  Screen Height: " + Screen.height);
     }
 }
